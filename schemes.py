f543b832468bf1f830abdb6dd89d811a97f495e1
fratric <fratric.p@gmail.com>
2025-02-18 13:11:02 +0100

    first version

--- schemes.py
+++ schemes.py
@@ -1,14 +1,102 @@
+from transaction_laboratory import transactionLaboratory
+from datetime import datetime, timedelta
+import random
+
+
+class random_behavior(transactionLaboratory):
+    
+    def __init__(self, N = 100) -> None:
+        super().__init__()
+        self.behavior_name = 'random'
+        self.N = N # Number of entities
+        self.compliant_accounts = ['C' + str(i) for i in range(N)]
+
+    def random_amount(self, min_amount = 10, max_amount = 500):
+        return round(random.uniform(min_amount, max_amount), 2)
+
+    def random_datetime(self, start, end):
+        delta = end - start
+        random_seconds = random.randint(0, int(delta.total_seconds()))
+        return start + timedelta(seconds=random_seconds)
+
+    def generate(self, k):
+        transactions = []
+        transaction_time = self.start
+        for _ in range(k):
+            from_node, to_node = self.random_accounts(accounts=self.compliant_accounts)
+            amount = self.random_amount()
+            transaction_time = self.random_time_step(transaction_time)
+            transactions.append((from_node, to_node, amount, transaction_time))
+        return self.toTable(transactions)
+
 class layering(transactionLaboratory):
-  def __init__(self, N, source_accounts, intermediate_accounts, target_accounts, capital = 10**6) -> None:
-     super().__init__(N)
-     self.capital = capital
-     self.source_accounts = source_accounts
-     self.intermediate_accounts = intermediate_accounts
-     self.target_accounts = target_accounts
 
-  def perform_fraud(self, start_date, end_date, step, camouflage = False):
-    G = nx.DiGraph()
-    fraudListG = []
+    def __init__(self, start, min_time_step = 0,  max_time_step = 10, source_accounts = 2, intermediate_accounts = 5, target_accounts = 1, capital = 10**6) -> None:
+        super().__init__(start, min_time_step, max_time_step)
+        self.capital = capital
+        
+        self.source_accounts = ['S' + str(i) for i in range(source_accounts)]
+        self.intermediate_accounts = ['I' + str(i) for i in range(intermediate_accounts)]
+        self.target_accounts = ['T' + str(i) for i in range(target_accounts)]
+        self.noncompliant_accounts = self.source_accounts + self.intermediate_accounts + self.target_accounts
+        self.start = start
+        self.end = end
+    
+    
+class round_tripping(transactionLaboratory):
 
+    def __init__(self, source_accounts = 10, intermediate_accounts = 25, target_accounts = 4, capital = 10**6) -> None:
+        super().__init__()
+        self.behavior_name = 'round_trip'
+        self.capital = capital
+        
+        self.source_accounts = ['S' + str(i) for i in range(source_accounts)]
+        self.intermediate_accounts = ['I' + str(i) for i in range(intermediate_accounts)]
+        self.target_accounts = ['T' + str(i) for i in range(target_accounts)]
+        self.noncompliant_accounts = self.source_accounts + self.intermediate_accounts + self.target_accounts
+    
+    def random_amount(self, min_amount = 100, max_amount = 200000):
+        return round(random.uniform(min_amount, max_amount), 2)
 
-    return fraudListG
+    def do_round_trip(self, amount, intermediate_steps):
+        transactions = []
+        transaction_time = self.random_time_step(self.start)
+
+        #generate a transaction between source and intermediate
+        from_node, to_node = self.random_accounts(accounts_from = self.source_accounts, accounts_to = self.intermediate_accounts)
+        transactions.append((from_node, to_node, amount, transaction_time))
+        
+        #generate transaction between intermediate accounts
+        for i in range(intermediate_steps):
+            transaction_time = self.random_time_step(transaction_time)
+            from_node = to_node
+            to_node = self.random_account(self.intermediate_accounts, except_account=from_node)
+            transactions.append((from_node, to_node, amount, transaction_time))
+        
+        #generate a transaction between intermediate and target
+        from_node = to_node
+        to_node = self.random_account(self.target_accounts, except_account=from_node)
+        transaction_time = self.random_time_step(transaction_time)
+        transactions.append((from_node, to_node, amount, transaction_time))
+        
+        return transactions
+
+    
+    def generate(self, max_intermediate_transactions = 12):
+        transactions = []
+        
+        remaining_capital = self.capital
+        while remaining_capital > 0:
+            amount = self.random_amount(max_amount = remaining_capital)
+            if remaining_capital - amount > 0:
+                remaining_capital = remaining_capital - amount
+            else:
+                amount = remaining_capital
+                remaining_capital = 0
+            
+            intermediate_steps = random.randint(1, max_intermediate_transactions)
+            transactions = transactions + self.do_round_trip(amount, intermediate_steps)
+        
+        return self.toTable(transactions)
+        
+        
--- transaction_laboratory.py
+++ transaction_laboratory.py
@@ -1,62 +1,62 @@
 import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
 import random
 from datetime import datetime, timedelta
 
-class transactionLaboratory():
+from abc import ABC, abstractmethod
 
-  def __init__(self, N = 100) -> None:
-    self.N = N # Number of entities
+class transactionLaboratory(ABC):
 
-  def getRandomAmount(self):
-    return round(random.uniform(10, 500), 2)  # Random transaction amount
+    def __init__(self) -> None:
+        self.start = datetime(2025, 3, 1, 8, 0, 0)  # March 1, 2024, 08:00 AM
+        self.min_time_step = 0
+        self.max_time_step = 10 #maximum time step in seconds between two transactions
 
-  def random_datetime(self, start, end):
-      delta = end - start
-      random_seconds = random.randint(0, int(delta.total_seconds()))
-      return start + timedelta(seconds=random_seconds)
+    @abstractmethod
+    def generate(self):
+        pass
 
-  def generate_transactions(self, start, end):
-    # Create a directed graph
-    G = nx.DiGraph()
-    # Add nodes (entities)
-    G.add_nodes_from(range(self.N))
-    # Randomly add directed edges (transactions) between entities
-    num_edges = random.randint(self.N, self.N * 2)  # Ensure at least N edges, but no more than N*2
-    for _ in range(num_edges):
-        from_node = random.randint(0, self.N-1)
-        to_node = random.randint(0, self.N-1)
-        while to_node == from_node:  # Avoid self-loops
-            to_node = random.randint(0, self.N-1)
-        amount = self.getRandomAmount()
-        transaction_time = self.random_datetime(start, end)
-        G.add_edge(from_node, to_node, weight=amount, time=transaction_time)
-    return G
+    def random_time_step(self, current_time):
+        return current_time + timedelta(seconds=random.randint(self.min_time_step, self.max_time_step))
 
-  def generate_transactions_in_time(self, start_date, end_date, step):
-    current_date = start_date
-    transaction_graphs = []
-    while current_date <= end_date:
-        transaction_graphs.append(self.generate_transactions(current_date, current_date + step))
-        current_date += step
-    return transaction_graphs
+    def random_account(self, accounts, except_account = None):
+        account = random.sample(accounts,1)[0]
+        if except_account is not None:
+            while account == except_account:  # Avoid self-loops
+                account = random.sample(accounts,1)[0]
+        return account
 
-  def plotTransactionGraph(self, G):
-    # Plot the graph
-    plt.figure(figsize=(10, 6))
-    pos = nx.forceatlas2_layout(G, scaling_ratio = 0.50, dissuade_hubs = True, seed = 42)  # Layout for better visualization
-    #pos = nx.circular_layout(G) # Layout for better visualization
-    edges = G.edges(data=True)
-    # Draw nodes and edges
-    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, edge_color='gray', arrows=True)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels={(u, v): f"${d['weight']}" for u, v, d in edges}, font_size=8)
-    plt.title("Transaction Graph Between" + str(self.N)  + "Entities")
-    plt.show()
+    def random_accounts(self, accounts = None, accounts_from = None, accounts_to = None):
+        if accounts is not None:
+            account_list_from = account_list_to = accounts
+        elif accounts_from is not None and accounts_to is not None:
+            account_list_from = accounts_from
+            account_list_to = accounts_to
+        else:
+            print("Warning: no account list provided")
 
-  #formats a transaction graph (or a sequence of graphs) into a table
-  def toTable(self, arg):
-    if isinstance(arg, nx.DiGraph):
-      arg = [arg]
-    dfs = []
-    for G in arg:
-      dfs.append(nx.to_pandas_edgelist(G))
-    return pd.concat(dfs)
+        from_node = self.random_account(account_list_from)
+        to_node = self.random_account(account_list_to, except_account=from_node)
+        
+        return from_node, to_node
+
+    def toTable(self, data):
+        if isinstance(data, list):
+            return pd.DataFrame(data, columns =['source', 'target', 'amount', 'time'])
+
+    def plotTransactionGraph(self, dataframe, edgeLables = False, title = 'Transaction graph'):
+        if isinstance(dataframe, pd.DataFrame):
+            G = nx.from_pandas_edgelist(dataframe, source='source', target='target', edge_attr=['amount', 'time'],create_using=nx.MultiDiGraph)
+
+        # Plot the graph
+        plt.figure(figsize=(10, 6))
+        pos = nx.forceatlas2_layout(G, scaling_ratio = 0.50, dissuade_hubs = True, seed = 42)  # Layout for better visualization
+        #pos = nx.circular_layout(G) # Layout for better visualization
+        edges = G.edges(data=True)
+        # Draw nodes and edges
+        nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=500, edge_color='gray', arrows=True)
+        if edgeLables:
+            nx.draw_networkx_edge_labels(G, pos, edge_labels={(u, v): f"${d['amount']}" for u, v, d in edges}, font_size=8)
+        plt.title(title)
+        plt.show()
